bf52df9dec378e427cfb0dcae108bd04a6cb5191
smlimonparvez <smlimonparvez@gmail.com>
2022-11-09 11:01:06 +0600

    Responsive issue fixed

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,16 +8,16 @@ const Hero = () => {
     return (
         <>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row ">
+                <div className="hero-content flex-col lg:flex-row p-0 lg:p-4">
                     <img src="https://cdn.pixabay.com/photo/2018/09/22/12/31/cat-3695213_960_720.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    <div className='sm: p-5'>
                         <h1 className="text-5xl font-bold">Welcome To Quiz Guru!</h1>
                         <p className="py-6">In the world of education there are many different ways to teach and to consolidate what has been learned. In years gone by, children were expected to memorise dates, formulae and figures by rote. But this method of teaching can be quite dull, to say the least! One tool becoming more common, especially in online education, is quizzes. In this guide we’ll find out the many reasons why quizzes are valuable in learning.</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
-            <div className='grid grid-cols-3 gap-3 my-20 ml-20'>
+            <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-3 my-20 ml-8 lg:ml-20'>
                 {
                     quizCards.data.map(quizCard => <QuizCard key={quizCard.id} quizCard={quizCard}></QuizCard>)
                 }
