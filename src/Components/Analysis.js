e451c665840ada37766c364f242cafaccc313314
smlimonparvez <smlimonparvez@gmail.com>
2022-11-09 13:43:00 +0600

    Rechart update

--- src/Components/Analysis.js
+++ src/Components/Analysis.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, XAxis, YAxis } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 const Analysis = () => {
     const chart = useLoaderData();
-    console.log(chart.data);
-      
+
     return (
-        <BarChart width={500} height={400} data={chart.data}>
-                <Bar dataKey="total" fill="#8884d8" />
-                <XAxis dataKey="name" />
-                <YAxis />
-            </BarChart>
+        <BarChart className='mx-auto' width={400} height={450} data={chart.data}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip/>
+        </BarChart>
     );
 };
 
